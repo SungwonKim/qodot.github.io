7fc57f53a9c573903fbab945238b46b43acda807
qodot <sdaewon12@gmail.com>
2016-05-07 17:17:06 +0900

    atom/rss 피드로 전달할 카테고리 url 세분화

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,8 @@ DEFAULT_LANG = 'en'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = 'category/%s/feed/atom.xml'
+CATEGORY_FEED_RSS = 'category/%s/feed/rss.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
