cc1d3e8f7f39c47142f0de93a68fb7570005296a
qodot <sdaewon12@gmail.com>
2016-05-07 17:18:20 +0900

    카테고리 atom/rss url 변경

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,8 +13,8 @@ DEFAULT_LANG = 'en'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_ATOM = 'category/%s/feed/atom.xml'
-CATEGORY_FEED_RSS = 'category/%s/feed/rss.xml'
+CATEGORY_FEED_ATOM = 'category/%s/feeds/atom.xml'
+CATEGORY_FEED_RSS = 'category/%s/feeds/rss.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
